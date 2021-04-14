e3f28d2191d4843f44e8b4dccfe80609f3be0f1c
hackcoderr <sachinkumarkashyap9025@gmail.com>
2021-04-14 16:54:10 +0530

    mv ip.txt cmd added

--- script.py
+++ script.py
@@ -2,5 +2,6 @@ import os
 os.system("sudo pip3 install ansible")
 os.system("sudo mkdir /etc/ansible")
 os.system("sudo mv ansible.cfg /etc/ansible/")
+os.system("sudo mv ip.txt /home/ec2-user/")
 os.system("sudo yum install https://dl.fedoraproject.org/pub/epel/epel-release-latest-8.noarch.rpm -y")
 os.system("sudo yum install sshpass -y")
