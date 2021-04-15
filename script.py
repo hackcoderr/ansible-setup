ab0117083eeab439c4fead0c64d75f5e738572a7
piyush <piyuinshort@gmail.com>
2021-04-15 10:21:54 +0000

    Feature Added: Installs ssh and sshpass in Ubuntu

--- script.py
+++ script.py
@@ -1,7 +1,18 @@
-import os
+import os,subprocess
+
 os.system("sudo pip3 install ansible")
 os.system("sudo mkdir /etc/ansible")
 os.system("sudo mv ansible.cfg /etc/ansible/")
 os.system("sudo mv ip.txt /home/ec2-user/")
-os.system("sudo yum install https://dl.fedoraproject.org/pub/epel/epel-release-latest-8.noarch.rpm -y")
-os.system("sudo yum install sshpass -y")
+
+operating_system = subprocess.getoutput('hostnamectl').split('\n')[6].split(':')[1]
+#print(operating_system)
+
+if 'Redhat' in operating_system or 'Amazon' in operating_system or 'Centos' in operating_system:
+        os.system("sudo yum install https://dl.fedoraproject.org/pub/epel/epel-release-latest-8.noarch.rpm -y")
+        os.system("sudo yum install sshpass -y")
+elif 'Ubuntu' in operating_system:
+        os.system("sudo apt-get update -y")
+        os.system("sudo apt-install openssh-server -y")
+        os.system("sudo systemctl enable ssh -y")
+        os.system("sudo apt-get install sshpass -y")
