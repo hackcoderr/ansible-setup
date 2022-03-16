db9dc20e09b470c1b80c2bc2319db801da768433
Sachin Kumar <56272723+hackcoderr@users.noreply.github.com>
2022-03-16 17:31:29 +0530

    Update script.py

--- script.py
+++ script.py
@@ -2,13 +2,13 @@ import os,subprocess
 
 os.system("sudo pip3 install ansible")
 os.system("sudo mkdir /etc/ansible")
-os.system("sudo mv ansible.cfg /etc/ansible/")
-os.system("sudo mv ip.txt /etc/ansible/")
+os.system("sudo mkdir -p /etc/ansible/hosts")
+os.system("sudo mv ec2.py ec2.ini ansible.cfg ip.txt /etc/ansible/")
 
 operating_system = subprocess.getoutput('hostnamectl').split('\n')[6].split(':')[1]
 #print(operating_system)
 
-if 'Redhat' in operating_system or 'Amazon' in operating_system or 'Centos' in operating_system:
+if 'Redhat' in operating_system or ':Amazon' in operating_system or 'Centos' in operating_system:
         os.system("sudo yum install https://dl.fedoraproject.org/pub/epel/epel-release-latest-8.noarch.rpm -y")
         os.system("sudo yum install sshpass -y")
 elif 'Ubuntu' in operating_system:
